809f9b0374be77c1ffc17d09624eaade80f060b7
Agnes Park <agnespark824@gmail.com>
2020-01-27 05:36:21 +0000

    Automatically backed up by Learn

--- p.rb
+++ p.rb
@@ -14,7 +14,7 @@ def key_for_min_value(name_hash)
   end
   new_hash = name_hash.select {|key, value| name_hash[key] == ref}
   end
- puts name_hash
+ puts new_hash
 end
 
 key_for_min_value(name_hash)
