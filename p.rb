8592fb96cefcadaaae8f287cf7e1b7adc1482e38
Agnes Park <agnespark824@gmail.com>
2020-01-27 05:26:48 +0000

    Automatically backed up by Learn

--- p.rb
+++ p.rb
@@ -1,19 +1,19 @@
 name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
-    if name_hash[key] == 25
-      puts key
+  if name_hash.empty? 
+    nil
+  else 
+    ref = 10000000000
+    name_hash.each do |key, value|
+      value
+      if value < ref
+        ref = value
+      end
     end
   end
+  name_hash.select do |key, value|
+    name_hash[key] == ref
+  end
+  puts name_hash
 end
-
-key_for_min_value(name_hash)
-
-#    name_hash.each do |key, value|
-#      if name_hash[key] == ref
-#        puts key
-#      end
-#    end
-#  end
-#end
