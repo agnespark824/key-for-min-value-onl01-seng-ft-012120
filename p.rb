6916003d13496247670c2388a248e2506a8549ef
Agnes Park <agnespark824@gmail.com>
2020-01-27 04:45:03 +0000

    Automatically backed up by Learn

--- p.rb
+++ p.rb
@@ -1,3 +1,19 @@
-ikea = {:chair => 25, :table => 85, :mattress => 450}
 
-puts ikea[1]
+def key_for_min_value(name_hash)
+  if name_hash.empty? 
+    nil
+  else 
+    ref = 10000000000
+    name_hash.each do |key, value|
+      value
+      if value < ref
+        ref = value
+      end
+    end
+    name_hash.each do |key, value|
+      if name_hash[key] == ref
+        puts key
+      end
+    end
+  end
+end
