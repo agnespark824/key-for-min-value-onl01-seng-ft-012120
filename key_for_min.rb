546846140e414f2879fbfbe79686e52f4c41b74b
Agnes Park <agnespark824@gmail.com>
2020-01-27 04:53:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,10 +13,9 @@ def key_for_min_value(name_hash)
       end
     end
   end
-    name_hash.each do |key, value|
-      if name_hash[key] == ref
-        puts key
-      end
+  name_hash.each do |key, value|
+    if name_hash[key] == ref
+      puts key
     end
   end
 end
